999edc4dee2657b4cd46ab66fabb56f441f6ae04
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-01-30 14:46:45 +0100

    Added form, but not functioning correctly

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//starting array
 const whatToBuy = [
   { item: "apples", amount: 3 },
   { item: "bread", amount: 1 },
@@ -5,10 +6,35 @@ const whatToBuy = [
   { item: "milk", amount: 2 },
 ];
 
+//testing whether it works to add new items to the array
 whatToBuy.push({ item: "fish", amount: 1 });
 
-whatToBuy.forEach((element) => {
-  document.getElementById(
-    "list"
-  ).innerHTML += `<div class="item">${element.item} - ${element.amount}</div>`;
+//function that displays the items from the array to the page
+const displayingTheList = () => {
+  whatToBuy.forEach((element) => {
+    document.getElementById(
+      "list"
+    ).innerHTML += `<div class="item">${element.item} - ${element.amount}</div>`;
+  });
+};
+
+//initial display when the page gets loaded
+displayingTheList();
+
+//accessing the button
+const button = document.getElementById("button");
+
+//when clicking the button, the values from text fields are converted into a new item of an array
+button.addEventListener("click", (event) => {
+  //prevents from refreshing the page when submit button clicked
+  event.preventDefault();
+  const item = document.querySelector('input[type="text"]').value;
+  const amount = document.querySelector('input[type="number"]').value;
+
+  whatToBuy.push({ item: item, amount: amount });
+
+  document.querySelector('input[type="text"]').value = "";
+  document.querySelector('input[type="number"]').value = "";
+
+  displayingTheList();
 });
