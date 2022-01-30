8843b6e0fe17c6bc101bba279ca80bc569e22a5a
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-01-30 13:03:58 +0100

    Added also an amount needed to buy

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const whatToBuy = ["apples", "bread", "bananas", "milk"];
-whatToBuy.push("fish");
+const whatToBuy = [
+  { item: "apples", amount: 3 },
+  { item: "bread", amount: 1 },
+  { item: "bananas", amount: 7 },
+  { item: "milk", amount: 2 },
+];
+
+whatToBuy.push({ item: "fish", amount: 1 });
 
 whatToBuy.forEach((element) => {
   document.getElementById(
     "list"
-  ).innerHTML += `<div class="item">${element}</div>`;
+  ).innerHTML += `<div class="item">${element.item} - ${element.amount}</div>`;
 });
