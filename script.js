2a2ed83984c72f358487b198e7889e4f711dc086
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-01-30 15:47:01 +0100

    Added the feature of deleting the items from the list

--- script.js
+++ script.js
@@ -9,32 +9,56 @@ const whatToBuy = [
 //testing whether it works to add new items to the array
 whatToBuy.push({ item: "fish", amount: 1 });
 
-//function that displays the items from the array to the page
-const displayingTheList = () => {
-  whatToBuy.forEach((element) => {
-    document.getElementById(
-      "list"
-    ).innerHTML += `<div class="item">${element.item} - ${element.amount}</div>`;
-  });
-};
-
-//initial display when the page gets loaded
-displayingTheList();
+//displaying the list on the page
+whatToBuy.forEach((element) => {
+  document.getElementById(
+    "list"
+  ).innerHTML += `<div class="item"><div>${element.item} - ${element.amount}</div><div class="cross">X</div></div>`;
+});
 
 //accessing the button
 const button = document.getElementById("button");
 
-//when clicking the button, the values from text fields are converted into a new item of an array
+//function that returns html
+const creatingHTML = (html) => {
+  const container = document.createElement("div");
+  container.innerHTML = html;
+  return container.firstChild;
+};
+
+//function that gives the crosses a power to delete
+const deletingItems = () => {
+  const crossArray = Array.from(document.querySelectorAll(".cross"));
+  crossArray.forEach((element) => {
+    element.addEventListener("click", () => {
+      element.parentNode.parentNode.removeChild(element.parentNode);
+    });
+  });
+};
+
+deletingItems();
+
+//when clicking the button, the values from text fields are converted into a new item on a page
 button.addEventListener("click", (event) => {
   //prevents from refreshing the page when submit button clicked
   event.preventDefault();
   const item = document.querySelector('input[type="text"]').value;
   const amount = document.querySelector('input[type="number"]').value;
 
-  whatToBuy.push({ item: item, amount: amount });
+  const input = creatingHTML(
+    `<div class="item"><div>${item} - ${amount}</div><div class="cross">X</div></div></div>`
+  );
 
+  //placing the new element into a list
+  document.getElementById("list").appendChild(input);
+
+  //deleting the text in the form fields
   document.querySelector('input[type="text"]').value = "";
   document.querySelector('input[type="number"]').value = "";
 
-  displayingTheList();
+  //giving a newly created item the power to delete itself
+  const cross = input.querySelector("div[class='cross']");
+  cross.addEventListener("click", () => {
+    cross.parentNode.parentNode.removeChild(cross.parentNode);
+  });
 });
